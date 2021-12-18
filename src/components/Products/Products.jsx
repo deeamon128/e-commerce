b2f0373a7219f73721089ddd0fa1c2e99c07c313
deeamon128 <andreeambobariu@gmail.com>
2021-12-18 18:58:48 +0000

    navbar padding

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Product from './Product/Product';
 import { Grid } from '@material-ui/core';
 
+import useStyles from './styles';
+
 const products = [
   { id: 1, name: 'Shoes', description:'Running shoes', price: '£10' },
   { id: 2, name: 'Laptop', description:'Luxury Laptop', price: '£20'},
@@ -10,14 +12,19 @@ const products = [
 ]
 
 const Products = () => {
+  const classes = useStyles();
   return (
-    <Grid container justifyContent="center" spacing={4}>
-      {products.map((product) => (
-        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-          <Product product={product}/>
+    <main className={classes.content}>
+      <div className={classes.toolbar}>
+        <Grid container justifyContent="center" spacing={4}>
+          {products.map((product) => (
+            <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+              <Product product={product}/>
+            </Grid>
+          ))}
         </Grid>
-      ))}
-    </Grid>
+      </div>
+    </main>
   )
 }
 
