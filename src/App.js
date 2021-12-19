df70f7ceef6881168246fc9bc08549293c287530
deeamon128 <andreeambobariu@gmail.com>
2021-12-19 13:20:28 +0000

    Add items to cart

--- src/App.js
+++ src/App.js
@@ -5,21 +5,33 @@ import Products from "./components/Products/Products";
 
 function App() {
   const [products, setProducts] = useState([])
+  const [cart, setCart] = useState({})
 
   const fetchProduct = async () => {
     const { data } = await commerce.products.list();
     setProducts(data);
   }
 
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  }
+
+  const handleAddToCart = async (productID, quantity) => {
+    const item = await commerce.cart.add(productID, quantity);
+    setCart(item.cart)
+  }
+
   useEffect(() => {
     fetchProduct();
+    fetchCart();
   }, [])
-console.log(products)
+
+  console.log(cart)
 
   return (
     <div>
-      <Navbar />
-      <Products products={products}/>
+      <Navbar totalItems={cart.total_items}/>
+      <Products products={products} onAddToCart={handleAddToCart}/>
     </div>
   );
 }
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -4,7 +4,7 @@ import { AddShoppingCart } from '@material-ui/icons';
 import useStyles from './styles';
 
 
-const Product = ( { product } ) => {
+const Product = ( { product, onAddToCart } ) => {
   const classes = useStyles();
 
   
@@ -22,12 +22,10 @@ const Product = ( { product } ) => {
             {product.formatted_with_symbol}
           </Typography>
         </div>
-        <Typography variant="body2" color="textSecondary">
-          {product.description}
-        </Typography>
+        <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" />
       </CardContent>
       <CardActions disableSpacing className={classes.cardActions}>
-        <IconButton aria-label="Add to Cart">
+        <IconButton aria-label="Add to Cart" onClick={() => onAddToCart(product.id, 1)}>
           <AddShoppingCart />
         </IconButton>
       </CardActions>
