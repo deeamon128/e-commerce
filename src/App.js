5ae182c328fdb7ecf4bc0026209f61b026aaad29
deeamon128 <andreeambobariu@gmail.com>
2021-12-19 21:01:13 +0000

    show the cart items

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { commerce } from './lib/commerce';
 import Navbar from "./components/Navbar/Navbar";
 import Products from "./components/Products/Products";
+import Cart from "./components/Cart/Cart";
+
 
 function App() {
   const [products, setProducts] = useState([])
@@ -26,12 +28,11 @@ function App() {
     fetchCart();
   }, [])
 
-  console.log(cart)
-
   return (
     <div>
       <Navbar totalItems={cart.total_items}/>
       <Products products={products} onAddToCart={handleAddToCart}/>
+      <Cart cart={cart}/>
     </div>
   );
 }
---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Container, Typography, Button, Grid } from '@material-ui/core';
+import CartItem from '../Cart/CartItem/CartItem'
+import useStyles from './styles';
+
+const Cart = ({ cart }) => {
+  const classes = useStyles();
+  const isEmpty = !cart.line_items;
+
+  const EmptyCart = () => (
+    <Typography variant="subtitle1">You have no items in your shopping cart, start adding some!</Typography>
+  );
+
+  const FilledCart = () => (
+    <>
+      <Grid container spacing={3}>
+        {cart.line_items.map((item) => (
+          <Grid item xs={12} sm={4} key={item.id}>
+            <CartItem />
+          </Grid>
+        ))}
+      </Grid>
+      <div className={classes.cardDetails}>
+        <Typography variant="h4">Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
+        <div>
+          <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" >Empty cart</Button>
+          <Button className={classes.checkoutButton} to="/checkout" size="large" type="button" variant="contained" color="primary">Checkout</Button>
+        </div>
+      </div>
+    </>
+  );
+
+  console.log(cart)
+
+  return (
+    <Container>
+      <div className={classes.tooldbar}/>
+      <Typography className={classes.title} variant="h3">Your Shopping Cart</Typography>
+      {isEmpty ? <EmptyCart /> : <FilledCart />}
+    </Container>
+  )
+}
+
+export default Cart
