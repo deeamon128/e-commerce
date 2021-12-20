247ed4abdde72a8d467eebd74ae04df92eb73062
deeamon128 <andreeambobariu@gmail.com>
2021-12-20 23:10:03 +0000

    import and mix form hook with material ui

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
+import FormInput from "../CheckoutForm/CustomTextField"
 
 const AddressForm = () => {
+  const methods = useForm();
   return (
-    <div>
-      address form
-    </div>
+    <>
+      <Typography variant="h6" gutterBottom>Shipping Address
+      <FormProvider {...methods}>
+        <form onSubmit=''>
+          <Grid container spacing={3}>
+            <FormInput required name="firstName" label="First name"/>
+            <FormInput required name="lastName" label="Last name" />
+            <FormInput required name="address1" label="Address line 1" />
+            <FormInput required name="email" label="Email" />
+            <FormInput required name="city" label="City" />
+            <FormInput required name="zip" label="Postal code" />
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Country</InputLabel>
+              <Select value={} fullWidth >
+                  <MenuItem key={} value={}>
+                  </MenuItem>
+              </Select>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Country</InputLabel>
+              <Select value={} fullWidth >
+                  <MenuItem key={} value={}>
+                  </MenuItem>
+              </Select>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Country</InputLabel>
+              <Select value={} fullWidth >
+                  <MenuItem key={} value={}>
+                  </MenuItem>
+              </Select>
+            </Grid>
+          </Grid>
+        </form>
+      </FormProvider>
+      </Typography>
+    </>
   )
 }
 
--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,7 +1,10 @@
 import React, { useState} from 'react';
 import { CssBaseline, Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 
+
 import useStyles from './styles';
+import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
 
 const steps = ['Shipping address', 'Payment details']
 
@@ -10,10 +13,16 @@ const Checkout = () => {
   const [activeStep, setActiveStep] = useState(0);
   const classes = useStyles();
 
-  const Form = () => activeStep === 0
+  const Form = () => ( activeStep === 0
     ? <AddressForm />
-    : <PaymentForm />
+    : <PaymentForm /> 
+  );
 
+    const Confirmation = () => (
+      <div>
+        Confirmation
+      </div>
+    );
   return (
     <>
       <div className={classes.toolbar} />
@@ -27,7 +36,7 @@ const Checkout = () => {
               </Step>
             ))}
           </Stepper>
-          {activeStep === steps.lenght ? <Confirmation /> : <Form />}
+          {activeStep === steps.length ? <Confirmation /> : <Form />}
         </Paper>
       </main>
     </>
