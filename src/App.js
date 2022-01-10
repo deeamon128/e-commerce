0a2a041b45b63b355d220c3b2e3bc6f28d5c188c
deeamon128 <andreeambobariu@gmail.com>
2022-01-10 19:46:59 +0000

    mobile view

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import Products from "./components/Products/Products";
 import Cart from "./components/Cart/Cart";
 import Checkout from "./components/CheckoutForm/Checkout/Checkout"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
- 
+
 
 function App() {
-  const [products, setProducts] = useState([])
-  const [cart, setCart] = useState({})
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
 
   const fetchProduct = async () => {
     const { data } = await commerce.products.list();
@@ -43,21 +46,39 @@ function App() {
     setCart(cart);
   };
 
+  const refreshCart = async () => {
+    const newCart = await commerce.cart.refresh();
+
+    setCart(newCart);
+  };
+
+  const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+    try {
+      const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+
+      setOrder(incomingOrder);
+
+      refreshCart();
+    } catch (error) {
+      setErrorMessage(error.data.error.message);
+    }
+  };
+
   useEffect(() => {
     fetchProduct();
     fetchCart();
   }, [])
 
-  console.log(cart)
+  const handleDrawerToggle = () => setMobileOpen(!mobileOpen);
 
   return (
     <div>
       <BrowserRouter>
-      <Navbar totalItems={cart.total_items}/>
+      <Navbar totalItems={cart.total_items} handleDrawerToggle={handleDrawerToggle}/>
       <Routes>
-        <Route path="/" element={<Products products={products} onAddToCart={handleAddToCart} />} />
+        <Route path="/" element={<Products products={products} onAddToCart={handleAddToCart}  handleUpdateCartQty />} />
         <Route path="cart" element={<Cart cart={cart} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} onEmptyCart={handleEmptyCart}/>}/>
-        <Route path="/checkout" element={<Checkout cart={cart}/>}/>
+        <Route path="/checkout" element={<Checkout cart={cart} order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage} />}/>
       </Routes>
       </BrowserRouter>
     </div>
